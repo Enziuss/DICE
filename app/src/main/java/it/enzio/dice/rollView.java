3cb87afe9e5aece1f8c9d27b6350882dd7724933
Enziuss <lorenzo.carrai.29@gmail.com>
2015-04-13 10:07:12 +0200

    some changes

--- app/src/main/java/it/enzio/dice/rollView.java
+++ app/src/main/java/it/enzio/dice/rollView.java
@@ -2,7 +2,6 @@ package it.enzio.dice;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
@@ -59,53 +58,40 @@ public class rollView extends Activity implements View.OnClickListener, View.OnT
 			if (locked){
 				lockNumber = 1;
 				locked = false;
-				Log.d("","unlocked");
 				lock.setBackground(getResources().getDrawable(R.drawable.unlocked));
 			} else {
 				lockNumber = Integer.parseInt(number.getText().toString());
 				locked = true;
 				lock.setBackground(getResources().getDrawable(R.drawable.locked));
-				Log.d("","locked");
 			}
 		}
 
 		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
 			if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                int x = Integer.parseInt(number.getText().toString());
+                x+= 1;
+                number.setText(Integer.toString(x));
 				return false; // Right to left
 			}  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-				return false; // Left to right
+                int x = Integer.parseInt(number.getText().toString());
+                if (x == 2){
+                    YoYo.with(Techniques.Shake)
+                        .duration(700)
+                        .playOn(findViewById(R.id.fr));
+                    return false;
+                }else {
+                    x -= 1;
+                    number.setText(Integer.toString(x));
+                    return false; // Left to right
+                }
 			}
 			if(e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
-				if (Integer.parseInt(number.getText().toString()) == 0 && !locked){
-					YoYo.with(Techniques.Shake)
-							.duration(700)
-							.playOn(findViewById(R.id.fr));
-					return false;
-				}
-				if (locked) {
-					Random random = new Random();
-					int rnd = random.nextInt(lockNumber);
-					YoYo.with(Techniques.FlipOutX)
-							.duration(700)
-							.playOn(findViewById(R.id.textView));
-					number.setText(Integer.toString(rnd));
-					YoYo.with(Techniques.FlipInX)
-							.duration(700)
-							.playOn(findViewById(R.id.textView));
-				}else{
-					Random random = new Random();
-					int rnd = random.nextInt(Integer.parseInt(number.getText().toString()));
-					YoYo.with(Techniques.FlipOutX)
-							.duration(700)
-							.playOn(findViewById(R.id.textView));
-					number.setText(Integer.toString(rnd));
-					YoYo.with(Techniques.FlipInX)
-							.duration(700)
-							.playOn(findViewById(R.id.textView));
-				}
+                int x = Integer.parseInt(number.getText().toString());
+                x+= 10;
+                number.setText(Integer.toString(x));
 				return false; // Bottom to top
 			}  else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
-				number.setText(Integer.toString(1));
+				number.setText(Integer.toString(2));
 				return false; // Top to bottom
 			}
 			return false;
@@ -117,7 +103,6 @@ public class rollView extends Activity implements View.OnClickListener, View.OnT
 
 		@Override
 		public boolean onDoubleTap(MotionEvent e) {
-			Log.d("test", e.toString());
 			int x = Integer.parseInt(number.getText().toString());
 			x+= 5;
 			number.setText(Integer.toString(x));
@@ -131,10 +116,22 @@ public class rollView extends Activity implements View.OnClickListener, View.OnT
 
 		@Override
 		public boolean onSingleTapConfirmed(MotionEvent e) {
-				int x = Integer.parseInt(number.getText().toString());
-				x += 1;
-				number.setText(Integer.toString(x));
-				return super.onSingleTapConfirmed(e);
+            if (locked) {
+                Random random = new Random();
+                int rnd = random.nextInt(lockNumber) + 1;
+                number.setText(Integer.toString(rnd));
+                YoYo.with(Techniques.FlipInX)
+                        .duration(700)
+                        .playOn(findViewById(R.id.textView));
+            }else{
+                Random random = new Random();
+                int rnd = random.nextInt(Integer.parseInt(number.getText().toString()) + 1);
+                number.setText(Integer.toString(rnd));
+                YoYo.with(Techniques.FlipInX)
+                        .duration(700)
+                        .playOn(findViewById(R.id.textView));
+            }
+			return super.onSingleTapConfirmed(e);
 		}
 	}
 
